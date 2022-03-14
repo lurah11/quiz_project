e1e6a76bf2de84f2951b39a42f876bef9a837d0e
印尼泗水飲料廠-Indrajid Prayoga 襄理 <qc.surabaya-02@asia.honchuan.com.tw>
2022-03-14 19:21:29 +0700

    sssss

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -129,9 +129,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static_project')
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static_cdn','static_root')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static_cdn','media_root')
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static_cdn','static_root')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static_cdn','media_root')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
